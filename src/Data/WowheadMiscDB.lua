28f1297726658fbfb81e9434082952592abc7221
Chris Turner <peavers@gmail.com>
2025-03-05 07:41:51 -0600

    Database update: update-wowheadmiscdb

--- src/Data/WowheadMiscDB.lua
+++ src/Data/WowheadMiscDB.lua
@@ -2,7 +2,7 @@ local addonName, addonTable = ...
 addonTable.WowheadMiscDB = addonTable.WowheadMiscDB or {}
 
 local talentData = {
-	updated = "2025-03-05 11:32:23",
+	updated = "2025-03-05 13:13:05",
 
 	[1] = {
 		specs = {
