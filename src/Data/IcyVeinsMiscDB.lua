fa67148d633f0472e2900c012c120fb9ad990c8e
Chris Turner <peavers@gmail.com>
2025-03-05 16:27:59 -0600

    Database update: update-icyveinsmiscdb

--- src/Data/IcyVeinsMiscDB.lua
+++ src/Data/IcyVeinsMiscDB.lua
@@ -2,7 +2,7 @@ local addonName, addonTable = ...
 addonTable.IcyVeinsMiscDB = addonTable.IcyVeinsMiscDB or {}
 
 local talentData = {
-	updated = "2025-03-05 13:13:05",
+	updated = "2025-03-05 22:02:28",
 
 	[2] = {
 		specs = {
