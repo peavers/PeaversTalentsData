70e6d6e36ffe26ade829ebe3d897feab4fb9959b
Chris Turner <peavers@gmail.com>
2025-06-18 10:11:56 -0700

    Database update: update-wowheadmythicdb

--- src/Data/WowheadMythicDB.lua
+++ src/Data/WowheadMythicDB.lua
@@ -116,6 +116,16 @@ local talentData = {
 					talentString = "CAQAAAAAAAAAAAAAAAAAAAAAAAA2mZmxYMGzMjZ2MbzMzMzAAAAAAAAAAAwilZbwMzgZhxMGjxYhtZamJWGwMDAKAzystBGb2AA",
 				},
 			},
+			[257] = {
+				[0] = {
+					label = "Oracle Mythic | Oracle",
+					talentString = "CEQAAAAAAAAAAAAAAAAAAAAAAwYAAAAAAAMbGGjxMMjZGzMzYbmZGAAAAglZZ2GMzMYWYWmxgBLsxMzUzCAmZBLEGzyAasALGjlB",
+				},
+				[1] = {
+					label = "Archon Mythic | Archon",
+					talentString = "CEQAAAAAAAAAAAAAAAAAAAAAAwYAAAAAAAMbGGjxMMjZGzMzYZmZGAAAAAzysNsNzMYWYWmxgxYhNMzUzCAwMLWmMbGAyYwmxYZA",
+				},
+			},
 			[258] = {
 				[0] = {
 					label = "Mythic | Archon",
