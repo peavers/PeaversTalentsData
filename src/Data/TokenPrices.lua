dea88b0bd2ec7717874efa3b4b8f408ff1bddf69
Chris Turner <peavers@gmail.com>
2025-12-04 14:30:57 -0600

    Data update: currency-update-tokenprices-2025-12-04-203055

---
+++ src/Data/TokenPrices.lua
@@ -0,0 +1,36 @@
+-- Auto-generated token price data
+-- Last updated: 2025-12-04T20:30:55.922969062Z
+
+PeaversCurrencyData = PeaversCurrencyData or {}
+
+-- WoW Token prices across regions
+PeaversCurrencyData.TokenPrices = {
+  lastUpdated = "2025-12-04T20:30:55.922969062Z",
+
+  regions = {
+    EU = {
+      goldPrice = 410966,  -- Gold cost of a token
+      realPrice = 20,      -- EUR cost of a token
+      currency = "EUR",
+      goldValue = 0.0000486658, -- EUR value of 1 gold
+    },
+    TW = {
+      goldPrice = 668532,  -- Gold cost of a token
+      realPrice = 500,      -- TWD cost of a token
+      currency = "TWD",
+      goldValue = 0.0007479074, -- TWD value of 1 gold
+    },
+    KR = {
+      goldPrice = 275309,  -- Gold cost of a token
+      realPrice = 22000,      -- KRW cost of a token
+      currency = "KRW",
+      goldValue = 0.0799102100, -- KRW value of 1 gold
+    },
+    US = {
+      goldPrice = 312357,  -- Gold cost of a token
+      realPrice = 20,      -- USD cost of a token
+      currency = "USD",
+      goldValue = 0.0000640293, -- USD value of 1 gold
+    },
+  }
+}
